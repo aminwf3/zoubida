9db53fa49cb3f0f17811fdad5fcd27a418491880
unknown <hachmi.aminpro@gmail.com>
2018-08-02 14:04:28 +0200

    dev: created new Controller and view - HomeController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,12 +6,12 @@ use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController
 {
-    public function index()
+    public function index():Response
     {
         $tab = array("toto" =>22, "tata" =>11);
         return new Response(
             json_encode($tab)
         );
     }
-    
+
 }
