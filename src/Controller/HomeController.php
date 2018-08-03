0fc73267a0cd9d268e83f7ab49aade81164e1a3d
unknown <hachmi.aminpro@gmail.com>
2018-08-03 10:51:25 +0200

    dev: finish template register and login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,13 +12,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            "user" => [
-                "Nom"=> "Bob",
-                "Prenom" => "Patrick",
-                "avatar" => "https://cdn-s-www.ledauphine.com/images/BE7AB5BD-E580-45C4-B811-7236702E837F/LDL_V0_12/bob-l-eponge-et-son-ami-patrick-l-etoile-de-mer-capture-d-ecran-1508313090.jpg",
-            ]
-        ]);
+        // return $this->render('home/index.html.twig', [
+        //     'controller_name' => 'HomeController',
+        //     "user" => [
+        //         "Nom"=> "Bob",
+        //         "Prenom" => "Patrick",
+        //         "avatar" => "https://cdn-s-www.ledauphine.com/images/BE7AB5BD-E580-45C4-B811-7236702E837F/LDL_V0_12/bob-l-eponge-et-son-ami-patrick-l-etoile-de-mer-capture-d-ecran-1508313090.jpg",
+        //     ]
+        // ]);
+    
+        return $this->render('offline/signup.html.twig');
     }
 }
+
